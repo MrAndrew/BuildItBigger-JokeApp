54462cca649e3024063c2a2dd22fcba18692e12c
MrAndrew7of9 <mr.andrew.7of9@gmail.com>
2018-07-20 15:36:50 +0800

    updated espresso test

--- app/src/androidTest/java/com/udacity/gradle/testing/DadJokesAsyncTaskAndroidTest.java
+++ app/src/androidTest/java/com/udacity/gradle/testing/DadJokesAsyncTaskAndroidTest.java
@@ -21,12 +21,17 @@ import com.udacity.gradle.builditbigger.EndpointsAsyncTask;
 import com.udacity.gradle.builditbigger.MainActivity;
 import com.udacity.gradle.builditbigger.R;
 
+import java.util.ArrayList;
+
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.core.IsNull.notNullValue;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
@@ -44,16 +49,13 @@ public class DadJokesAsyncTaskAndroidTest {
         Espresso.registerIdlingResources(mIdlingResource);
     }
 
-    @NonNull String dadJoke = new String();
-
     @Test
     public void testVerifyJokeReturned() throws InterruptedException {
         onView(withId(R.id.tell_joke_btn)).perform(click());
 
-        DadJokes myDadJoke = new DadJokes();
-        dadJoke = myDadJoke.getJoke();
-
-        onView(withId(R.id.fullscreen_content)).check(matches(withText(dadJoke)));
+        //not sure how to match one of the many possible jokes since it's randomized, so just making
+        //sure it's not null and something is returned
+        onView(withId(R.id.fullscreen_content)).check(matches(not(withText(""))));
     }
 
     @After
